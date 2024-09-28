3142e3d6d7564cb19670ba2e130d46f0b31e3c13
Prem <premdasvm8@gmail.com>
2024-09-29 01:49:26 +0530

    refactor: code linted

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -5,39 +5,43 @@ import { Pressable } from 'react-native';
 
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
 function TabBarIcon(props: { name: React.ComponentProps<typeof FontAwesome>['name']; color: string }) {
-    return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
+	return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
 }
 
 export default function TabLayout() {
-    return (
-        <Tabs
-            screenOptions={{
-                headerShown: true,
-            }}
-        >
-            <Tabs.Screen
-                name="index"
-                options={{
-                    title: 'Tab One',
-                    tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-                    headerRight: () => (
-                        <Link href="/modal" asChild>
-                            <Pressable>
-                                {({ pressed }) => (
-                                    <FontAwesome name="info-circle" size={25} style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }} />
-                                )}
-                            </Pressable>
-                        </Link>
-                    ),
-                }}
-            />
-            <Tabs.Screen
-                name="two"
-                options={{
-                    title: 'Tab Two',
-                    tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-                }}
-            />
-        </Tabs>
-    );
+	return (
+		<Tabs
+			screenOptions={{
+				headerShown: true,
+			}}
+		>
+			<Tabs.Screen
+				name="index"
+				options={{
+					title: 'Tab One',
+					tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+					headerRight: () => (
+						<Link href="/modal" asChild>
+							<Pressable>
+								{({ pressed }) => (
+									<FontAwesome
+										name="info-circle"
+										size={25}
+										style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+									/>
+								)}
+							</Pressable>
+						</Link>
+					),
+				}}
+			/>
+			<Tabs.Screen
+				name="two"
+				options={{
+					title: 'Tab Two',
+					tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+				}}
+			/>
+		</Tabs>
+	);
 }
--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -2,31 +2,31 @@ import React, { useEffect } from 'react';
 import { View, Animated, StyleSheet } from 'react-native';
 
 export default function TabTwoScreen() {
-    const fadeAnim = new Animated.Value(0);
+	const fadeAnim = new Animated.Value(0);
 
-    useEffect(() => {
-        Animated.timing(fadeAnim, {
-            toValue: 1,
-            duration: 2000,
-            useNativeDriver: true,
-        }).start();
-    }, []);
+	useEffect(() => {
+		Animated.timing(fadeAnim, {
+			toValue: 1,
+			duration: 2000,
+			useNativeDriver: true,
+		}).start();
+	}, []);
 
-    return (
-        <View style={styles.container}>
-            <Animated.Text style={[styles.title, { opacity: fadeAnim }]}>Tab Two</Animated.Text>
-        </View>
-    );
+	return (
+		<View style={styles.container}>
+			<Animated.Text style={[styles.title, { opacity: fadeAnim }]}>Tab Two</Animated.Text>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    title: {
-        fontSize: 20,
-        fontWeight: 'bold',
-    },
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	title: {
+		fontSize: 20,
+		fontWeight: 'bold',
+	},
 });
