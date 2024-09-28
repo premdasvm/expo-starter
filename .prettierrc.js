79db1e48e8b4a1026107ff2d4cab51bf58ac6550
Prem <premdasvm8@gmail.com>
2024-09-29 01:39:31 +0530

    feat: eslint, prettier added and configured

---
+++ .prettierrc.js
@@ -0,0 +1,16 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+
+module.exports = {
+	printWidth: 120,
+	tabWidth: 4,
+	useTabs: true,
+	semi: true,
+	singleQuote: true,
+	trailingComma: "all",
+	bracketSpacing: true,
+	arrowParens: "always",
+	endOfLine: "auto",
+};
